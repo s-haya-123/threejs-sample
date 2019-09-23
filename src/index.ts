a840be510c269eb6b7ed29d8edc7731d09f50c6c
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-09-23 15:53:41 +0900

    experiment Image predict

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,29 @@ function loadImg(id: string): tf.Tensor<tf.Rank> | undefined {
       }).cast('float32').div(tf.scalar(255));
 }
 
-console.log('start');
 tf.loadLayersModel('../model/classifyIllust/model.json').then(m => {    
     const batched = loadImg('img');
     const batched2 = loadImg('img2');
+
+    const img = new Image();
+    img.src = './assets/44_CuZy55AUAAQjNJU.jpg';
+    const resizeImg = tf.image.resizeBilinear(tf.browser.fromPixels(img,3),[224,224]);
+    const batched3 = tf.tidy(() => {
+        return resizeImg.expandDims(0);
+      }).cast('float32').div(tf.scalar(255));
+
     if( !!batched && !!batched2) {
         console.log(batched);
         console.log('result');
-        const batchImg = batched.concat(batched2);
-        (m.predict(batchImg) as any).print();
+        // const batchImg = batched.concat(batched2);
+        // (m.predict(batched) as any).print();
+        const predict = (m.predict(batched) as tf.Tensor<tf.Rank>).data().then( (data: any)=>console.log(data));
+        console.log(predict);
         // !!batched ? (m.predict(batched) as any).print():{};
         // !!batched2 ? (m.predict(batched2) as any).print():{};
+    }
+    if ( !!batched3 ) {
+        (m.predict(batched3)  as tf.Tensor<tf.Rank>).print();
     }
 },
 (e)=>{
