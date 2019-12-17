12932b80beab1e7e741e2fd5dcc9ab4ece8af30c
hayamizu <shintaro.hayamizu@kabuku.co.jp>
2019-12-17 18:23:36 +0900

    set fire on marker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as handTrack from 'handtrackjs';
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { Fire } from 'three/examples/jsm/objects/Fire.js';
 // import * as THREE from 'three';
 
 
@@ -65,6 +65,7 @@ renderer.domElement.style.position = 'absolute'
 renderer.domElement.style.top = '0px'
 renderer.domElement.style.left = '0px'
 document.body.appendChild( renderer.domElement );
+
 // array of functions for the rendering loop
 var onRenderFcts= [];
 // init scene and camera
@@ -80,6 +81,37 @@ scene.add(camera);
 const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 light.position.set(0, 1, 0);
 scene.add(light);
+
+
+var plane = new THREE.PlaneBufferGeometry( 20, 20 );
+var fire = new Fire( plane, {
+    textureWidth: 512,
+    textureHeight: 512,
+    debug: false
+} );
+var params = {
+    color1: '#ffffff',
+    color2: '#ffa000',
+    color3: '#000000',
+    colorBias: 0.7,
+    burnRate: 2.04,
+    diffuse: 1.07,
+    viscosity: 0,
+    expansion: 1,
+    swirl: 50.0,
+    drag: 0.35,
+    airSpeed: 12.0,
+    windX: 0.0,
+    windY: 0.75,
+    speed: 442.0,
+    massConservation: false
+};
+params.Single = function () {
+    fire.clearSources();
+    fire.addSource( 0.5, 0.1, 0.1, 1.0, 0.0, 1.0 );
+};
+
+
 ////////////////////////////////////////////////////////////////////////////////
 //          handle arToolkitSource
 ////////////////////////////////////////////////////////////////////////////////
@@ -127,6 +159,10 @@ onRenderFcts.push(function(){
 })
 const makerRoot = new THREE.Group();
 scene.add(makerRoot);
+fire.position.set(0,0,0);
+
+params.Single();
+updateAll();
 // init controls for camera
 var markerControls = new THREEx.ArMarkerControls(arToolkitContext, makerRoot, {
     type : 'pattern',
@@ -165,6 +201,7 @@ loader.load('../assets/Sword.obj',
     group.position.set(0,0,0);
     group.scale.set(0.03,0.03,0.03);
     makerRoot.add(group);
+    makerRoot.add( fire );
 });
 // onRenderFcts.push(function(delta){
 //     mesh.rotation.x += Math.PI*delta;
@@ -192,3 +229,112 @@ requestAnimationFrame(function animate(nowMsec){
 })
 
 // setTimeout(start,100);
+
+function updateAll() {
+
+    updateColor1( params.color1 );
+    updateColor2( params.color2 );
+    updateColor3( params.color3 );
+    updateColorBias( params.colorBias );
+    updateBurnRate( params.burnRate );
+    updateDiffuse( params.diffuse );
+    updateViscosity( params.viscosity );
+    updateExpansion( params.expansion );
+    updateSwirl( params.swirl );
+    updateDrag( params.drag );
+    updateAirSpeed( params.airSpeed );
+    updateWindX( params.windX );
+    updateWindY( params.windY );
+    updateSpeed( params.speed );
+    updateMassConservation( params.massConservation );
+
+}
+function updateColor1( value ) {
+
+    fire.color1.set( value );
+
+}
+
+function updateColor2( value ) {
+
+    fire.color2.set( value );
+
+}
+
+function updateColor3( value ) {
+
+    fire.color3.set( value );
+
+}
+
+function updateColorBias( value ) {
+
+    fire.colorBias = value;
+
+}
+
+function updateBurnRate( value ) {
+
+    fire.burnRate = value;
+
+}
+
+function updateDiffuse( value ) {
+
+    fire.diffuse = value;
+
+}
+
+function updateViscosity( value ) {
+
+    fire.viscosity = value;
+
+}
+
+function updateExpansion( value ) {
+
+    fire.expansion = value;
+
+}
+
+function updateSwirl( value ) {
+
+    fire.swirl = value;
+
+}
+
+function updateDrag( value ) {
+
+    fire.drag = value;
+
+}
+
+function updateAirSpeed( value ) {
+
+    fire.airSpeed = value;
+
+}
+
+function updateWindX( value ) {
+
+    fire.windVector.x = value;
+
+}
+
+function updateWindY( value ) {
+
+    fire.windVector.y = value;
+
+}
+
+function updateSpeed( value ) {
+
+    fire.speed = value;
+
+}
+
+function updateMassConservation( value ) {
+
+    fire.massConservation = value;
+
+}
